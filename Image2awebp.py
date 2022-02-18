dbb9045bb86c2cd112397d1cb7c18636d2e43fd4
Barney Young <byoung03@qub.ac.uk>
2022-02-18 18:20:10 +0000

    Implement the use of vidstab
    Update README
    Update .gitignore

--- Image2awebp.py
+++ Image2awebp.py
@@ -1,13 +1,19 @@
 import argparse, sys, os, time, ntpath
+from tempfile import tempdir
 from PIL import Image
 from PIL import features
+from vidstab import VidStab
+import cv2
+import numpy as np
 
 # Setup and parse command line arguments
 parser = argparse.ArgumentParser(description="Program creates an animated .webp from a single input image by splitting it into four segments")
 parser.add_argument("-I", "--input", help="Set input filepath. Supports .png, .jpg/.jpeg, and .tiff", required=True)
 parser.add_argument("-O", "--output", help="Set output directory (Default = .\\Output)", default=str(os.path.dirname(__file__)) + "\\Output")
 parser.add_argument("-D", "--duration", help="Set duration of frames in milliseconds (Default = 200ms)", default=200, type=int)
-parser.add_argument("-C", "--crop", help="Set crop factor of each frame (Default = 100 = no crop)", default=100, type=int)
+parser.add_argument("-C", "--crop", help="Set the crop factor of each frame (Applied before stabilisation) (Default = 100 = no crop)", default=100, type=int)
+parser.add_argument("-S", "--stabilise", help="Use AI to stabilise the frames", action="store_true")
+parser.add_argument("-R", "--border", help="Size of border in output. Positive values pad, negative values crop (Default = 0)", default=0, type=int)
 parser.add_argument("-G", "--gif", help="Save the animation as a .gif file instead of .webp", action="store_true")
 parser.add_argument("-B", "--boomerang", help="Use 'boomerang' style looping", action="store_true")
 
@@ -17,6 +23,11 @@ print(("Using input file: %s") % (args.input))
 print(("Output directory: %s") % (args.output))
 print(("Using frame duration: %sms") % (args.duration))
 print(("Using crop factor: %s%%") % (args.crop))
+if args.stabilise:
+    print(("Stabilisation with a border size of: %s") % (args.border))
+else:
+    print("No stabilisation")
+
 if args.gif:
     print("Saving as .gif")
 else:
@@ -29,9 +40,9 @@ else:
 
 # Load input image
 try:
-    fullImg = Image.open(args.input)
-except:
-    print("ERROR - Cannot read input file")
+    fullImg = np.array(Image.open(args.input))
+except Exception as ex:
+    print("ERROR - " + str(ex))
     sys.exit(2)
 
 # Check output does not end in a slash and remove if it does
@@ -53,34 +64,70 @@ if not os.path.isdir(args.output):
 
 # Split image into four
 
+# Pillow
+# 0,0 = upper left
 # Image.crop(x1, y1, x2, y2)
 # Image.crop(left, upper, right, lower)
 # Image.size[0] = width
 # Image.size[1] = height
-subImgWidth = (fullImg.size[0] / 2) * (args.crop / 100)
-subImgHeight = (fullImg.size[1] / 2) * (args.crop / 100)
-xOffset = ((fullImg.size[0] / 2) - subImgWidth) / 2
-yOffset = ((fullImg.size[1] / 2) - subImgHeight) / 2
+
+# numpy
+# 0,0 = upper left
+# img[y1:y2, x1:x2]
+# x1,y1 = left,upper | x2,y2 = right,lower
+# img.shape[0] = height
+# img.shape[1] = width
+subImgWidth = (fullImg.shape[1] / 2) * (args.crop / 100)
+subImgHeight = (fullImg.shape[0] / 2) * (args.crop / 100)
+xOffset = ((fullImg.shape[1] / 2) - subImgWidth) / 2
+yOffset = ((fullImg.shape[0] / 2) - subImgHeight) / 2
 
 # Top Left
-frame1 = fullImg.crop((xOffset, yOffset, xOffset + subImgWidth, yOffset + subImgHeight))
+# frame1 = fullImg.crop((xOffset, yOffset, xOffset + subImgWidth, yOffset + subImgHeight))
+frame1 = fullImg[int(yOffset):int(yOffset + subImgHeight), int(xOffset):int(xOffset + subImgWidth)].copy()
 # Bottom Left
-frame2 = fullImg.crop((xOffset, (fullImg.size[1]/2) + yOffset, xOffset + subImgWidth, (fullImg.size[1]/2) + yOffset + subImgHeight))
+# frame2 = fullImg.crop((xOffset, (fullImg.shape[0]/2) + yOffset, xOffset + subImgWidth, (fullImg.shape[0]/2) + yOffset + subImgHeight))
+frame2 = fullImg[int((fullImg.shape[0]/2) + yOffset):int((fullImg.shape[0]/2) + yOffset + subImgHeight), int(xOffset):int(xOffset + subImgWidth)].copy()
 # Bottom Right
-frame3 = fullImg.crop(((fullImg.size[0]/2) + xOffset, (fullImg.size[1]/2) + yOffset, (fullImg.size[0]/2) + xOffset + subImgWidth, (fullImg.size[1]/2) + yOffset + subImgHeight))
+# frame3 = fullImg.crop(((fullImg.shape[1]/2) + xOffset, (fullImg.shape[0]/2) + yOffset, (fullImg.shape[1]/2) + xOffset + subImgWidth, (fullImg.shape[0]/2) + yOffset + subImgHeight))
+frame3 = fullImg[int((fullImg.shape[0]/2) + yOffset):int((fullImg.shape[0]/2) + yOffset + subImgHeight), int((fullImg.shape[1]/2) + xOffset):int((fullImg.shape[1]/2) + xOffset + subImgWidth)].copy()
 # Top Right
-frame4 = fullImg.crop(((fullImg.size[0]/2) + xOffset, yOffset, (fullImg.size[0]/2) + xOffset + subImgWidth, yOffset + subImgHeight))
+# frame4 = fullImg.crop(((fullImg.shape[1]/2) + xOffset, yOffset, (fullImg.shape[1]/2) + xOffset + subImgWidth, yOffset + subImgHeight))
+frame4 = fullImg[int(yOffset):int(yOffset + subImgHeight), int((fullImg.shape[1]/2) + xOffset):int((fullImg.shape[1]/2) + xOffset + subImgWidth)].copy()
 
-# generate and save awebp/gif to file
-
+# Create sequences
 if args.boomerang:
     sequence = [frame1, frame2, frame3, frame4, frame3, frame2]
 else:
     sequence = [frame1, frame2, frame3, frame4]
     
-if args.gif:
-    sequence[0].save((args.output + "\\" + ntpath.basename(args.input.split(".")[0]) + "_Animation.gif"), save_all = True, append_images = sequence[1:], duration = args.duration, loop = 0)
-    print("File saved to " + args.output + "\\" + ntpath.basename(args.input.split(".")[0]) + "_Animation.gif")
+# Stabilise frames if selected
+if args.stabilise:
+    # Setup stabilisation
+    stabiliser = VidStab()
+
+    outSequence = []
+    for frame in sequence:
+        # Stabilise the frame
+        stabFrame = stabiliser.stabilize_frame(input_frame=frame, border_type="black", border_size=args.border, smoothing_window=1)
+        # Need to bin first frame from the stabiliser as it will be a black frame
+        if frame is frame1:
+            # Convert stabilised frame to pillow format and append to the output sequence
+            outSequence.append(Image.fromarray(np.uint8(frame)).convert('RGB'))
+        else:
+            # Convert stabilised frame to pillow format and append to the output sequence
+            outSequence.append(Image.fromarray(np.uint8(stabFrame)).convert('RGB'))
 else:
-    sequence[0].save((args.output + "\\" + ntpath.basename(args.input.split(".")[0]) + "_Animation.webp"), save_all = True, append_images = sequence[1:], duration = args.duration)
-    print("File saved to " + args.output + "\\" + ntpath.basename(args.input.split(".")[0]) + "_Animation.webp")
+    outSequence = []
+    for frame in sequence:
+        # Convert frames to pillow format and append to the output sequence
+        outSequence.append(Image.fromarray(np.uint8(frame)).convert('RGB'))
+
+# generate and save awebp/gif to the output dir
+if args.gif:
+    outPath = args.output + "\\" + ntpath.basename(args.input.split(".")[0]) + "_Animation.gif"
+else:
+    outPath = args.output + "\\" + ntpath.basename(args.input.split(".")[0]) + "_Animation.webp"
+
+outSequence[0].save(outPath, save_all = True, append_images = outSequence[1:], duration = args.duration, loop = 0)
+print("File saved to " + outPath)
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Image2awebp
 
-A script to turn a single image into an animated .webp (or .gif) file by splitting the image into four frames.
+A script to turn a single image into an animated .webp (or .gif) file by splitting the image into four frames and aligning the frames automatically.
 
 Frames are in the order:
 
@@ -31,21 +31,26 @@ The program uses the [Pillow](https://pillow.readthedocs.io/en/stable/installati
 ## Usage
 
 ```txt
-Image2awebp.py [-h] -I INPUT [-O OUTPUT] [-D DURATION] [-C CROP] [-G] [-B]
+Image2awebp.py [-h] -I INPUT [-O OUTPUT] [-D DURATION] [-C CROP] [-S] [-R BORDER] [-G] [-B]
 
 optional arguments:
   -h, --help            show this help message and exit
 
   -I INPUT, --input INPUT
                         Set input filepath. Supports .png, .jpg/.jpeg, and .tiff
-  
+
   -O OUTPUT, --output OUTPUT
                         Set output directory (Default = .\Output)
-  
+
   -D DURATION, --duration DURATION
                         Set duration of frames in milliseconds (Default = 200ms)
-  
-  -C CROP, --crop CROP  Set crop factor of each frame (Default = 100 = no crop)
+
+  -C CROP, --crop CROP  Set the crop factor of each frame (Applied before stabilisation) (Default = 100 = no crop)
+
+  -S, --stabilise       Use AI to stabilise the frames
+
+  -R BORDER, --border BORDER
+                        Size of border in output. Positive values pad, negative values crop (Default = 0)
 
   -G, --gif             Save the animation as a .gif file instead of .webp
 
@@ -86,8 +91,18 @@ python Image2awebp.py -I "InputImage.tiff" -G --duration 250
 
 - Requires [Pillow](https://pillow.readthedocs.io/en/stable/installation.html) version 8.2.0 or higher
 
----
+  ```sh
+  pip install Pillow
+  ```
+
+- Requires [VidStab](https://github.com/AdamSpannbauer/python_video_stab) version 1.7.4 or higher
+
+  - Requires [OpenCV](https://pypi.org/project/opencv-contrib-python/) version 4.5.5.62 or higher
+
+    ```sh
+    pip install vidstab[cv2]
+    ```
 
 ## TODO
 
-- Perhaps some AI image aligning
+- Image aligning currently does not function as intended
